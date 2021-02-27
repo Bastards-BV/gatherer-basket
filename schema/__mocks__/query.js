3ba57175d34aef4f3ff4c1b60535b839f67a4fd8
Maarten Bicknese <maarten.bicknese@gmail.com>
2021-02-27 17:48:39 +0100

    feat: new query to find single item

--- schema/__mocks__/query.js
+++ schema/__mocks__/query.js
@@ -3,5 +3,6 @@ const { MockList } = require("apollo-server");
 module.exports = {
   Query: () => ({
     items: () => new MockList([0, 5]),
+    item: (entity, { id }) => (id === "non-existent" ? null : entity),
   }),
 };
--- schema/query.js
+++ schema/query.js
@@ -3,5 +3,6 @@ const { gql } = require("apollo-server");
 module.exports = gql`
   type Query {
     items: [Item]
+    item(id: String!): Item
   }
 `;
